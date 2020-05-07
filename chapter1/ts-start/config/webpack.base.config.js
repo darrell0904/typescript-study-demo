ec1113d5aef623b378fe79f5bbe81d167d89fe81
傅佳威 <fujiawei@nears.cn>
2020-05-07 23:44:19 +0800

    feat(start) : typescript 编译配置代码

---
+++ chapter1/ts-start/config/webpack.base.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        'app': './src/index.ts'
+    },
+    output: {
+        filename: '[name].[chunkhash:8].js'
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/]
+                    }
+                }],
+                exclude: /node_modules/
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+}
